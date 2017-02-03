e4750287c07084b986ebb3cf174d637c63c7cbd1
Murat Başeren <muratbaseren@hotmail.com>
2017-02-03 23:30:55 +0300

    Add project files.

---
+++ AngularSamples/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AngularSamples.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public JsonResult GetSampleData(int count)
+        {
+            List<object> list = new List<object>();
+
+            for (int i = 0; i < count; i++)
+            {
+                list.Add(
+                    new
+                    {
+                        Name = FakeData.NameData.GetFirstName(),
+                        Surname = FakeData.NameData.GetSurname(),
+                        Age = FakeData.NumberData.GetNumber(10, 100)
+                    });
+            }
+
+            return Json(list, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
