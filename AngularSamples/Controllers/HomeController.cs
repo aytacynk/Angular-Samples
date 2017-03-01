ba3cfb8efa5dc672373e8fa4c3dce0347b13040c
Murat Başeren <muratbaseren@hotmail.com>
2017-03-02 02:36:54 +0300

    Add new sample page. I used folder system and created directive, service, controller, filter, module for a sample.

--- AngularSamples/Controllers/HomeController.cs
+++ AngularSamples/Controllers/HomeController.cs
@@ -8,28 +8,10 @@ namespace AngularSamples.Controllers
 {
     public class HomeController : Controller
     {
+
         public ActionResult Index()
         {
             return View();
-        }
-
-
-        public JsonResult GetSampleData(int count)
-        {
-            List<object> list = new List<object>();
-
-            for (int i = 0; i < count; i++)
-            {
-                list.Add(
-                    new
-                    {
-                        Name = FakeData.NameData.GetFirstName(),
-                        Surname = FakeData.NameData.GetSurname(),
-                        Age = FakeData.NumberData.GetNumber(10, 100)
-                    });
-            }
-
-            return Json(list, JsonRequestBehavior.AllowGet);
         }
     }
 }
